379668da5af8d2e09f5a34f4361f4479b4f68bc7
biac <biac@nifty.com>
2020-01-12 16:33:34 +0900

    FM: Changed class name and method name

--- FontManager/FontManager/FontSize.cs
+++ FontManager/FontManager/FontSize.cs
@@ -4,23 +4,26 @@ using System.Windows.Documents;
 
 namespace BluewaterSoft.WpfUtil
 {
-  public class FontManager : DependencyObject
+  /// <summary>
+  /// <para xml:lang="ja">XAML において、 相対値でフォントサイズを記述できるようにする。</para>
+  /// <para xml:lang="en">Allows you to describe font size by relative value when writing XAML code.</para>
+  /// </summary>
+  public class FontSize : DependencyObject
   {
-    #region BaseSize
+    #region Base attached property
     private static readonly double DefaultBaseSize = SystemFonts.MessageFontSize;
     // It defaults to SystemFonts.MessageFontSize which has a default value of 12.
     // https://stackoverflow.com/a/48624542
 
     /// <summary>
-    /// BaseSize 添付プロパティ:
-    /// RelativeSize の基準となる FontSize を設定します。
-    /// 単位付記も可能です(px, pt, in, cm)。
+    /// <para xml:lang="ja">Relative サイズの基準となる Font サイズです。 単位記号も付記できます (px, pt, in, cm)。</para>
+    /// <para xml:lang="en">Font size on which Relative size is based. Unit symbol (px, pt, in, cm) may be added.</para>
     /// </summary>
-    public static readonly DependencyProperty BaseSizeProperty
+    public static readonly DependencyProperty BaseProperty
       = DependencyProperty.RegisterAttached(
-          "BaseSize",
+          "Base",
           typeof(string),
-          typeof(FontManager),
+          typeof(FontSize),
           new FrameworkPropertyMetadata(
             defaultValue: DefaultBaseSize.ToString(),
             FrameworkPropertyMetadataOptions.Inherits
@@ -30,10 +33,12 @@ namespace BluewaterSoft.WpfUtil
           )
         );
 
-    public static void SetBaseSize(FrameworkElement element, string value)
-      => element.SetValue(BaseSizeProperty, value);
-    public static string GetBaseSize(FrameworkElement element)
-      => (element != null) ? (string)element.GetValue(BaseSizeProperty)
+    /// <summary>Setter for BaseProperty</summary>
+    public static void SetBase(FrameworkElement element, string value)
+      => element.SetValue(BaseProperty, value);
+    /// <summary>Getter for BaseProperty</summary>
+    public static string GetBase(FrameworkElement element)
+      => (element != null) ? (string)element.GetValue(BaseProperty)
                            : DefaultBaseSize.ToString();
 
     private static void BaseSizePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -43,22 +48,22 @@ namespace BluewaterSoft.WpfUtil
         return;
 
       string baseSizeString = e.NewValue as string;
-      double relativeSize = GetRelativeSize(element);
+      double relativeSize = GetRelative(element);
       SetFontSize(element, baseSizeString, relativeSize);
     }
     #endregion
 
 
-    #region RelativeSize
+    #region Relative attached property
     /// <summary>
-    /// RelativeSize 添付プロパティ:
-    /// フォントサイズを RelativeSize を基準とした比率で設定します。
+    /// <para xml:lang="ja">Base サイズの倍率で表されるフォントサイズ。</para>
+    /// <para xml:lang="en">Font size expressed as a magnification of the Base size.</para>
     /// </summary>
-    public static readonly DependencyProperty RelativeSizeProperty
+    public static readonly DependencyProperty RelativeProperty
       = DependencyProperty.RegisterAttached(
-          "RelativeSize",
+          "Relative",
           typeof(double),
-          typeof(FontManager),
+          typeof(FontSize),
           new FrameworkPropertyMetadata(
             defaultValue: double.NaN,
             FrameworkPropertyMetadataOptions.Inherits
@@ -70,10 +75,12 @@ namespace BluewaterSoft.WpfUtil
           )
         );
 
-    public static void SetRelativeSize(FrameworkElement element, double value)
-      => element.SetValue(RelativeSizeProperty, value);
-    public static double GetRelativeSize(FrameworkElement element)
-      => (element != null) ? (double)element.GetValue(RelativeSizeProperty)
+    /// <summary>Setter for RelativeProperty</summary>
+    public static void SetRelative(FrameworkElement element, double value)
+      => element.SetValue(RelativeProperty, value);
+    /// <summary>Getter for RelativeProperty</summary>
+    public static double GetRelative(FrameworkElement element)
+      => (element != null) ? (double)element.GetValue(RelativeProperty)
                            : double.NaN;
 
     private static void RelativeSizePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -83,21 +90,20 @@ namespace BluewaterSoft.WpfUtil
         return;
 
       double relativeSize = (double)e.NewValue;
-      SetFontSize(element, GetBaseSize(element), relativeSize);
+      SetFontSize(element, GetBase(element), relativeSize);
     }
     #endregion
 
-    #region AbsoluteSize
+    #region Absolute attached property
     /// <summary>
-    /// AbsoluteSize 添付プロパティ:
-    /// フォントサイズを絶対値で設定します。
-    /// 本来の FontSize プロパティと同じ意味です。
+    /// <para xml:lang="ja">実際の値で表されるフォントサイズ。本来の FontSize プロパティと同じ意味です。</para>
+    /// <para xml:lang="en">Font size expressed as a real value. Same meaning as the traditional FontSize property.</para>
     /// </summary>
-    public static readonly DependencyProperty AbsoluteSizeProperty
+    public static readonly DependencyProperty AbsoluteProperty
       = DependencyProperty.RegisterAttached(
-          "AbsoluteSize",
+          "Absolute",
           typeof(string),
-          typeof(FontManager),
+          typeof(FontSize),
           new FrameworkPropertyMetadata(
             defaultValue: null,
             FrameworkPropertyMetadataOptions.AffectsMeasure
@@ -108,10 +114,12 @@ namespace BluewaterSoft.WpfUtil
           )
         );
 
-    public static void SetAbsoluteSize(FrameworkElement element, string value)
-      => element.SetValue(AbsoluteSizeProperty, value);
-    public static string GetAbsoluteSize(FrameworkElement element)
-      => element?.GetValue(AbsoluteSizeProperty) as string;
+    /// <summary>Setter for AbsoluteProperty</summary>
+    public static void SetAbsolute(FrameworkElement element, string value)
+      => element.SetValue(AbsoluteProperty, value);
+    /// <summary>Getter for AbsoluteProperty</summary>
+    public static string GetAbsolute(FrameworkElement element)
+      => element?.GetValue(AbsoluteProperty) as string;
 
     private static void AbsoluteSizePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
@@ -157,6 +165,6 @@ namespace BluewaterSoft.WpfUtil
     private static readonly FontSizeConverter _fontSizeConverter
       = new FontSizeConverter();
     private static double s2d(string fontSize)
-     => (double)_fontSizeConverter.ConvertFromString(fontSize);
+      => (double)_fontSizeConverter.ConvertFromString(fontSize);
   }
 }
